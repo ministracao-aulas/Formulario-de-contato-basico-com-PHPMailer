a44851dee31fad46e93997411d7530121d21504d
Tiago França <dev@tiagofranca.com>
2022-06-07 08:36:25 -0300

    try/catch on index
    No ramo master
    Mudanças a serem submetidas:
    	modified:   index.php

--- index.php
+++ index.php
@@ -11,12 +11,19 @@ require_once __DIR__ . '/functions/__enabledFunctions.php';
 try {
     require_once __DIR__ . '/server.php';
 } catch (\Throwable $th) {
-    saveLog("\n" . __FILE__ . ':' . __LINE__ . "\n" . $th->getMessage(), 'error');
+    try {
+        saveLog("\n" . __FILE__ . ':' . __LINE__ . "\n" . $th->getMessage(), 'error');
+    } catch (\Throwable $th) {
+        if (DEBUG) {
+            throw $th;
+        }
+
+        die('An unexpected error has occurred. Please try again later.');
+    }
 
     if (DEBUG) {
-        echo '<pre>';
-        echo $th->getMessage();
-        echo '</pre>';
-        die;
+        throw $th;
     }
+
+    die('An unexpected error has occurred. Please try again later.');
 }
